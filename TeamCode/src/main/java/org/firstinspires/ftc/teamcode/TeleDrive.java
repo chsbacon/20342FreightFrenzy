4e1ca5b134fee20f6cfc178ed0d069dc95f372e7
afox2736 <afox2736@charlottesvilleschools.org>
2021-11-02 11:25:26 -0400

    Added DriveForwardOp

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleDrive.java
@@ -0,0 +1,53 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * This file is an example POV mode TeleOp
+ * This code is structured as a Linear OpMode
+ **/
+
+@TeleOp(name = "TeleDrive", group = "Examples")
+//@Disabled
+
+public class TeleDrive extends LinearOpMode{
+    private ElapsedTime runtime = new ElapsedTime();
+    HardwareMap2022 robot = new HardwareMap2022();
+
+    @Override
+    public void runOpMode(){
+        robot.init(hardwareMap);
+
+        robot.frontRightMotor.setDirection(DcMotor.Direction.REVERSE);
+        robot.backRightMotor.setDirection(DcMotor.Direction.REVERSE);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        while (opModeIsActive()){
+            //POV Mode driving (left stick go forward/back, right stick turn)
+            double drive = -gamepad1.left_stick_y;
+            double turn  =  gamepad1.right_stick_x;
+            double leftPower    = Range.clip(drive + turn, -1.0, 1.0) ;
+            double rightPower   = Range.clip(drive - turn, -1.0, 1.0) ;
+
+            robot.frontLeftMotor.setPower(leftPower);
+            robot.backLeftMotor.setPower(leftPower);
+            robot.frontRightMotor.setPower(rightPower);
+            robot.backRightMotor.setPower(rightPower);
+
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.addData("Motors", "left (%.2f), right (%.2f)", leftPower, rightPower);
+        }
+
+    }
+}
