a7aef8dd48b937b810e90d285f1a8012bc1aca75
afox2736 <91150771+afox2736@users.noreply.github.com>
2021-11-16 21:39:34 -0500

    Update EncoderAutoDrive.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAutoDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderAutoDrive.java
@@ -13,9 +13,9 @@ public class EncoderAutoDrive extends LinearOpMode{
     EncoderHMap             robot   = new EncoderHMap();
     private ElapsedTime     runtime = new ElapsedTime();
 
-    static final double     COUNTS_PER_MOTOR_REV = 560;
-    static final double     DRIVE_GEAR_REDUCTION = 2.0; //! ask gear ratio
-    static final double     WHEEL_DIAMETER_INCHES = 4.0; //! change
+    static final double     COUNTS_PER_MOTOR_REV = 537.6;
+    static final double     DRIVE_GEAR_REDUCTION = 2.0;   // output (wheel) speed / input (motor) speed
+    static final double     WHEEL_DIAMETER_INCHES = 4.0;
     static final double     COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) /
                                               (WHEEL_DIAMETER_INCHES * 3.1415);
     static final double DRIVE_SPEED = 0.3;
