e936a42336870aff4ede5d5334aaeb0a697e42cb
gabedamiani <gdamian2927@charlottesvilleschools.org>
2021-11-28 15:51:13 -0500

    inits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armFunc.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armFunc.java
@@ -1,33 +1,40 @@
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 public class armFunc {
-    private ElapsedTime runtime = new ElapsedTime();
-    EncoderHMap robot = new EncoderHMap();
+    EncoderHMap hwMap2 = null;
+    ElapsedTime runtime2 = null;
+
+    public void init(EncoderHMap ahwMap, ElapsedTime runtime) {
+        hwMap2 = ahwMap;
+        runtime2 = runtime;
+    }
     public void runIntakeMotor(double time, boolean deposit) {
-        runtime.reset();
+        runtime2.reset();
         if(deposit) {
-            while(runtime.seconds() < time) {
-                robot.intakeMotor.setPower(-1);
+            while(runtime2.seconds() < time) {
+                hwMap2.intakeMotor.setPower(-1);
             }
         } else {
-            while(runtime.seconds() < time) {
-                robot.intakeMotor.setPower(1);
+            while(runtime2.seconds() < time) {
+                hwMap2.intakeMotor.setPower(1);
             }
         }
     }
     public void armMove(int degrees) {
         double ticks = (degrees/360.0)*288;
-        robot.armMotor.setTargetPosition((int)ticks);
-        robot.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        robot.armMotor.setPower(1);
-        while(robot.armMotor.isBusy()) {
+        hwMap2.armMotor.setTargetPosition((int)ticks);
+        hwMap2.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        hwMap2.armMotor.setPower(1);
+        while(hwMap2.armMotor.isBusy()) {
             //Nothing needed here
         }
-        robot.armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        robot.armMotor.setPower(0);
+        hwMap2.armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hwMap2.armMotor.setPower(0);
     }
     public void armSet(int Setting) {
         switch(Setting) {
