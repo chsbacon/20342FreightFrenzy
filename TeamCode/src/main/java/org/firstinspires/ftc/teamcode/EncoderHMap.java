e552f681843b4bcecc0b9ce09cb8d5de75188eba
afox2736 <91150771+afox2736@users.noreply.github.com>
2021-11-28 14:47:39 -0500

    Create EncoderHMap.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderHMap.java
@@ -0,0 +1,57 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+/**
+ * Hardware map for robot with encoders
+**/
+
+public class EncoderHMap
+{
+    /* Public OpMode members. */
+    public DcMotor  leftMotor     = null;
+    public DcMotor  rightMotor    = null;
+    public DcMotor  armMotor      = null;
+    public DcMotor  carouselMotor = null;
+    public DcMotor  intakeMotor   = null;
+
+    /* local OpMode members. */
+    HardwareMap hwMap           =  null;
+    private ElapsedTime period  = new ElapsedTime();
+
+    /* Constructor */
+    public EncoderHMap(){
+    }
+
+    /* Initialize standard Hardware interfaces */
+    public void init(HardwareMap ahwMap) {
+        // Save reference to Hardware map
+        hwMap = ahwMap;
+
+        // Define and Initialize Motors
+        leftMotor     = hwMap.get(DcMotor.class, "LM");  //P0
+        rightMotor    = hwMap.get(DcMotor.class, "RM");  //P1
+        armMotor      = hwMap.get(DcMotor.class, "");  //P
+        carouselMotor = hwMap.get(DcMotor.class, "");  //P
+        intakeMotor   = hwMap.get(DcMotor.class, "");  //P
+        
+        //Reverse right motor
+        leftMotor.setDirection(DcMotor.Direction.FORWARD); 
+        rightMotor.setDirection(DcMotor.Direction.REVERSE);
+        
+        // Set all motors to zero power
+        leftMotor.setPower(0);
+        rightMotor.setPower(0);
+        armMotor.setPower(0);
+        carouselMotor.setPower(0);
+        intakeMotor.setPower(0);
+
+        // Set motors to run with encoders.
+        leftMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        rightMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+    }
+ }
