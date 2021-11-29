ea477c59f379a4e761a3d9744a57d12a2cf49116
gabedamiani <gdamian2927@charlottesvilleschools.org>
2021-11-29 16:25:38 -0500

    inits

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armFunc.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/armFunc.java
@@ -1,40 +1,41 @@
 package org.firstinspires.ftc.teamcode;
-
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.HardwareMap;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.util.ElapsedTime;
-
+import com.qualcomm.robotcore.util.Range;
 public class armFunc {
-    EncoderHMap hwMap2 = null;
-    ElapsedTime runtime2 = null;
+    EncoderHMap hwMap = null;
+    ElapsedTime runtime = null;
 
     public void init(EncoderHMap ahwMap, ElapsedTime runtime) {
-        hwMap2 = ahwMap;
-        runtime2 = runtime;
+        this.hwMap = ahwMap;
+        this.runtime = runtime;
     }
     public void runIntakeMotor(double time, boolean deposit) {
-        runtime2.reset();
+        runtime.reset();
         if(deposit) {
-            while(runtime2.seconds() < time) {
-                hwMap2.intakeMotor.setPower(-1);
+            while(runtime.seconds() < time) {
+                hwMap.intakeMotor.setPower(-1);
             }
         } else {
-            while(runtime2.seconds() < time) {
-                hwMap2.intakeMotor.setPower(1);
+            while(runtime.seconds() < time) {
+                hwMap.intakeMotor.setPower(1);
             }
         }
     }
     public void armMove(int degrees) {
         double ticks = (degrees/360.0)*288;
-        hwMap2.armMotor.setTargetPosition((int)ticks);
-        hwMap2.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
-        hwMap2.armMotor.setPower(1);
-        while(hwMap2.armMotor.isBusy()) {
+        hwMap.armMotor.setTargetPosition((int)ticks);
+        hwMap.armMotor.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+        hwMap.armMotor.setPower(1);
+        while(hwMap.armMotor.isBusy()) {
             //Nothing needed here
         }
-        hwMap2.armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
-        hwMap2.armMotor.setPower(0);
+        hwMap.armMotor.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
+        hwMap.armMotor.setPower(0);
     }
     public void armSet(int Setting) {
         switch(Setting) {
