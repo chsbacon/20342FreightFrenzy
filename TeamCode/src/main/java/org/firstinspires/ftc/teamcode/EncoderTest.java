0c3f0413481c4d0a4d2aa3475c441be834117579
afox2736 <91150771+afox2736@users.noreply.github.com>
2021-12-01 10:23:08 -0500

    Update EncoderTest.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/EncoderTest.java
@@ -47,10 +47,12 @@ public class EncoderTest extends LinearOpMode{
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
         encoderDrive(DRIVE_SPEED,  20,  20, 30.0);  // S1: Forward 47 Inches with 10 Sec timeout
-        //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        //encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
-
-        telemetry.addData("Path", "Complete");
+        telemetry.addData("Drive", "Complete");
+        telemetry.update();
+        
+        sleep(5000);
+        rotate(0.3, 90); 
+        telemetry.addData("Rotate", "Complete");
         telemetry.update();
     }
 
