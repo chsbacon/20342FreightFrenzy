e4c0356833bf9a786e2d1a659507f803e218e053
afox2736 <91150771+afox2736@users.noreply.github.com>
2021-12-01 10:25:12 -0500

    Update OpenCV_Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpenCV_Test.java
@@ -50,9 +50,9 @@ public class OpenCV_Test extends LinearOpMode {
         webcam.openCameraDeviceAsync(() -> webcam.startStreaming(CAMERA_WIDTH, CAMERA_HEIGHT, OpenCvCameraRotation.UPRIGHT));
 
         // Only if you are using ftcdashboard
-//        FtcDashboard dashboard = FtcDashboard.getInstance();
-//        telemetry = dashboard.getTelemetry();
-//        FtcDashboard.getInstance().startCameraStream(webcam, 10);
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = dashboard.getTelemetry();
+        FtcDashboard.getInstance().startCameraStream(webcam, 10);
 
         telemetry.update();
         waitForStart();
