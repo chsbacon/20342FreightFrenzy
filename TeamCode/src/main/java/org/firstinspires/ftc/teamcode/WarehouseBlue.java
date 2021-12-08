1476e66be7ad9d6ceeb94293bfb644e98ec8cabe
nbednar2929 <91289646+nbednar2929@users.noreply.github.com>
2021-12-08 16:51:42 -0500

    Create WarehouseBlue.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WarehouseBlue.java
@@ -0,0 +1,39 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+/**
+ * This file is a basic auto op template
+ * The code is structured as a LinearOpMode
+ **/
+
+@Autonomous(name="WarehouseBlue", group="Autonomous")
+//@Disabled
+
+public class WarehouseBlue extends AutoBasicOpMode {
+    private ElapsedTime runtime = new ElapsedTime();
+
+    @Override
+    public void runOpMode(){
+        robot.init(hardwareMap);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        waitForStart();
+        runtime.reset();
+
+        encoderDrive(.3, 36,36, 19);
+        encoderDrive(.3, -8.15,8.15,19);
+        armMove(120);
+        encoderDrive(.3, 24.45,-24.45, 19);
+        encoderDrive(.3, 50,50, 19);
+
+    }
+}
